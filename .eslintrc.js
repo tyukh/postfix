c4fdd0e881ddb23e63205b9991a9a0d8e17343aa
tyukh <roman.tyukh@gmail.com>
2022-11-01 20:16:29 +0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:@typescript-eslint/recommended",
+        "standard-with-typescript",
+        "prettier/@typescript-eslint",
+        "plugin:prettier/recommended"
+    ],
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
