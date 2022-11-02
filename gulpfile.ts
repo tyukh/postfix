4f34b79cf2ba1c7f4c0d3a28f58c59320d8c5335
tyukh <61107121+tyukh@users.noreply.github.com>
2022-11-02 16:09:41 +0300

    Adding gulpfile.ts

---
+++ gulpfile.ts
@@ -0,0 +1,15 @@
+"use strict";
+
+import gulp from "gulp";
+import ts from "gulp-typescript";
+import sourcemaps from "gulp-sourcemaps";
+
+const tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("default", function () {
+  return tsProject.src()
+    .pipe(sourcemaps.init())
+    .pipe(tsProject())
+    .js
+    .pipe(gulp.dest("./dist"));
+});
