cce7e07e90024e51d9e5d0f778eba8e07389abca
tyukh <roman.tyukh@gmail.com>
2022-11-10 17:38:51 +0300

    Daily fixup 2022.11.10

--- src/global.d.ts
+++ src/global.d.ts
@@ -5,65 +5,66 @@ declare function print(msg: string): void;
 declare function logError(error: Error, msg?: string): void;
 declare function printerr(msg: string): void;
 
-declare interface GjsExtensionUtils {
-  initTranslations: (domain?: string) => void;
-  getSettings: (schema?: string) => Gio.Settings;
-  openPrefs: () => void;
-  getCurrentExtension: () => GjsExtension;
-}
+declare namespace Gjs {
+  declare interface ExtensionUtils {
+    initTranslations: (domain?: string) => void;
+    getSettings: (schema?: string) => Gio.Settings;
+    openPrefs: () => void;
+    getCurrentExtension: () => GjsExtension;
+  }
 
-declare interface GjsExtensionMetadata {
-  uuid: string;
-  name: string;
-  'settings-schema'?: string;
-  'gettext-domain'?: string;
-}
+  declare interface ExtensionMetadata {
+    uuid: string;
+    name: string;
+    'settings-schema'?: string;
+    'gettext-domain'?: string;
+  }
 
-declare interface GjsExtension {
-  metadata: GjsExtensionMetadata;
-  uuid: string;
-  dir: Gio.File;
-  path: string;
-}
+  declare interface Extension {
+    metadata: GjsExtensionMetadata;
+    uuid: string;
+    dir: Gio.File;
+    path: string;
+  }
 
-declare interface GjsGiImports {
-  versions: {
-    [key: string]: string;
-  };
-  Adw: typeof import('@gi-types/adw1');
-  Clutter: typeof import('@gi-types/clutter');
-  Gio: typeof import('@gi-types/gio');
-  Glib: typeof import('@gi-types/glib');
-  GObject: typeof import('@gi-types/gobject');
-  Gtk: typeof import('@gi-types/gtk');
-  St: typeof import('@gi-types/st');
-}
+  declare interface Gi {
+    versions: {
+      [key: string]: string;
+    };
+    Adw: typeof import('@gi-types/adw');
+    Clutter: typeof import('@gi-types/clutter');
+    Gio: typeof import('@gi-types/gio');
+    Glib: typeof import('@gi-types/glib');
+    GObject: typeof import('@gi-types/gobject');
+    Gtk: typeof import('@gi-types/gtk');
+    St: typeof import('@gi-types/st');
+  }
 
-declare interface GjsMiscImports {
-  extensionUtils: GjsExtensionUtils;
-}
+  declare interface Misc {
+    extensionUtils: ExtensionUtils;
+  }
 
-// declare interface GjsUiImports {
-//   main: any;
-//   panelMenu: any;
-//   popupMenu: any;
-// }
+  declare interface Ui {
+    main: any;
+    panelMenu: any;
+    popupMenu: any;
+  }
 
-declare interface GjsGettextImports {
-  gettext: (msgid: string) => string;
-  ngettext: (msgid1: string, msgid2: string, n: number) => string;
-  domain: (domainName: string) => {
+  declare interface Gettext {
     gettext: (msgid: string) => string;
     ngettext: (msgid1: string, msgid2: string, n: number) => string;
-  };
+    domain: (domainName: string) => {
+      gettext: (msgid: string) => string;
+      ngettext: (msgid1: string, msgid2: string, n: number) => string;
+    };
+  }
+
+  declare interface Imports {
+    gi: Gi;
+    misc: Misc;
+    ui: Ui;
+    gettext: Gettext;
+  }
 }
 
-declare interface GjsImports {
-  gi: GjsGiImports;
-  misc: GjsMiscImports;
-  // ui: GjsUiImports;
-  ui: typeof imports.ui;
-  gettext: GjsGettextImports;
-}
-
-declare const imports: GjsImports;
+declare const imports: Gjs.Imports;
